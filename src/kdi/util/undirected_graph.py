fa2a816a58b6bcf57b90a46fd624ba9b692d5323
Derek G <45305531+dGrowl@users.noreply.github.com>
2024-08-29 18:34:43 -0700

    Add types for graph interaction

--- src/kdi/util/undirected_graph.py
+++ src/kdi/util/undirected_graph.py
@@ -1,19 +1,21 @@
 from collections import Counter, defaultdict
-from typing import DefaultDict, Iterable
+from typing import DefaultDict, Iterable, Union
 
-NodeWeights = Counter[str]
-KeyPair = tuple[str, str]
+Key = str
+KeyPair = tuple[Key, Key]
 KeyPairs = Iterable[KeyPair]
+KeySet = Union[set[Key], frozenset[Key]]
+NodeWeights = Counter[Key]
 
 
 class UndirectedGraph:
-	_weights: DefaultDict[str, NodeWeights]
+	_weights: DefaultDict[Key, NodeWeights]
 
 	def __init__(self):
 		self._weights = defaultdict(NodeWeights)
 
 	def __str__(self):
-		seen: set[str] = set()
+		seen: set[Key] = set()
 		lines: list[str] = []
 		for u, edges in self._weights.items():
 			for v, w in edges.items():
@@ -23,17 +25,17 @@ class UndirectedGraph:
 				seen |= {u, v}
 		return "{\n" + "\n".join(lines) + "\n}"
 
-	def __getitem__(self, key: str):
+	def __getitem__(self, key: Key):
 		return self._weights[key]
 
 	def clear(self):
 		self._weights.clear()
 
-	def add(self, u: str, v: str, amount: int):
+	def add(self, u: Key, v: Key, amount: int):
 		self._weights[u][v] += amount
 		self._weights[v][u] += amount
 
-	def increment(self, u: str, v: str):
+	def increment(self, u: Key, v: Key):
 		self.add(u, v, 1)
 
 	def increment_pairs(self, pairs: KeyPairs):
@@ -46,15 +48,15 @@ STRONG_FORCE = WEAK_FORCE * 1_000
 
 
 class MagneticGraph(UndirectedGraph):
-	_attractions: DefaultDict[str, set[str]]
-	_repulsions: DefaultDict[str, set[str]]
+	_attractions: DefaultDict[Key, set[Key]]
+	_repulsions: DefaultDict[Key, set[Key]]
 
 	def __init__(self):
 		super().__init__()
 		self._attractions = defaultdict(set)
 		self._repulsions = defaultdict(set)
 
-	def __getitem__(self, u: str):
+	def __getitem__(self, u: Key):
 		return self._weights[u]
 
 	def clear(self):
@@ -62,7 +64,7 @@ class MagneticGraph(UndirectedGraph):
 		self._attractions.clear()
 		self._repulsions.clear()
 
-	def reset_polarity(self, u: str, v: str):
+	def reset_polarity(self, u: Key, v: Key):
 		if u in self._attractions:
 			self.add(u, v, STRONG_FORCE)
 			self._attractions[u].discard(v)
@@ -72,7 +74,7 @@ class MagneticGraph(UndirectedGraph):
 			self._repulsions[u].discard(v)
 			self._repulsions[v].discard(u)
 
-	def attract(self, u: str, v: str):
+	def attract(self, u: Key, v: Key):
 		self.reset_polarity(u, v)
 		self.add(u, v, -STRONG_FORCE)
 		self._attractions[u].add(v)
@@ -82,7 +84,7 @@ class MagneticGraph(UndirectedGraph):
 		for u, v in pairs:
 			self.attract(u, v)
 
-	def repel(self, u: str, v: str):
+	def repel(self, u: Key, v: Key):
 		self.reset_polarity(u, v)
 		self.add(u, v, STRONG_FORCE)
 		self._repulsions[u].add(v)
