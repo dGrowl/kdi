c79e5f3f83350c63ede3029474ce1fed893c6a82
Derek G <45305531+dGrowl@users.noreply.github.com>
2024-10-20 15:19:46 -0700

    Use API message to avoid expired reply token

--- src/kdi/teams/round_reminder.py
+++ src/kdi/teams/round_reminder.py
@@ -4,6 +4,7 @@ from lightbulb.ext import tasks
 import hikari
 import lightbulb
 
+from ..bot import kdi
 from ..util import get_config_value
 
 TWENTY_MINUTES_SECS = 60 * 20
@@ -12,15 +13,17 @@ ROUND_REMINDER_CRON = get_config_value("teams", "round_reminder_cron")
 
 
 class RoundReminder:
-	_ctx: Optional[lightbulb.SlashContext]
+	_channel_id: Optional[hikari.Snowflakeish]
+	_prev_message: Optional[hikari.Message]
 	_role: Optional[hikari.Role]
 
 	def __init__(self):
-		self._ctx = None
+		self._channel_id = None
+		self._prev_message = None
 		self._role = None
 
 	def start(self, ctx: lightbulb.SlashContext):
-		self._ctx = ctx
+		self._channel_id = ctx.channel_id
 		self._role = ctx.options["reminder-role"]
 		self.task.start()
 
@@ -28,13 +31,17 @@ class RoundReminder:
 		self.task.cancel()
 
 	async def send(self):
-		if not self._ctx or not self._role:
+		if self._channel_id is None or self._role is None:
 			return
-		await self._ctx.respond(
+		self._prev_message = await kdi.rest.create_message(
+			self._channel_id,
 			f"{self._role.mention} Please prepare for the next round!",
-			delete_after=TWENTY_MINUTES_SECS,
+			role_mentions=True,
 		)
 
-	@tasks.task(tasks.CronTrigger(ROUND_REMINDER_CRON), max_executions=12)
+	@tasks.task(tasks.CronTrigger(ROUND_REMINDER_CRON), max_executions=8)
 	async def task(self):
+		if self._prev_message is not None:
+			await self._prev_message.delete()
+			self._prev_message = None
 		await self.send()
